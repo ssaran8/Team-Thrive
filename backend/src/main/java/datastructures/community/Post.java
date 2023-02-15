83f7fd785a0978799b5a504ff1df6a4664e5e170
Mamoun Chami <mamoun.chami@epfl.ch>
2023-02-14 20:02:06 -0800

    updated user class with strings which will be the respective document ids

--- backend/src/main/java/datastructures/community/Post.java
+++ backend/src/main/java/datastructures/community/Post.java
@@ -1,26 +1,28 @@
 package datastructures.community;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
 
     private final String authorID;
-    private final List<String> commentIds;
-    private final Date datePosted;
-    private final List<String> likesUserIds;
     private final String taskId;
     private final String text;
 
-    public Post(String authorID, List<String> comments, Date datePosted, List<String> likes, String task, String text) {
-        this.authorID = authorID;
-        this.commentIds = comments;
-        this.datePosted = datePosted;
-        this.likesUserIds = likes;
-        this.taskId = task;
-        this.text = text;
-    }
+    private final List<String> commentIds;
+    private final List<String> likesUserIds;
 
+    private final LocalDateTime datePosted;
+
+    public Post(String authorID, String taskId, String text, List<String> commentIds, List<String> likesUserIds, LocalDateTime datePosted) {
+        this.authorID = authorID;
+        this.taskId = taskId;
+        this.text = text;
+        this.commentIds = commentIds;
+        this.likesUserIds = likesUserIds;
+        this.datePosted = datePosted;
+    }
 
     public void addLike(String u){
         likesUserIds.add(u);
@@ -46,7 +48,7 @@ public class Post {
         return commentIds;
     }
 
-    public Date getDatePosted() {
+    public LocalDateTime getDatePosted() {
         return datePosted;
     }
 
