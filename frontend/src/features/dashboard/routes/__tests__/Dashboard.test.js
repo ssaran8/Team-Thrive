ee52018adbe0662fbafd1918de065c7d202f8116
Simon Studen <simonstuden@gmail.com>
2023-02-21 15:58:12 -0800

    Fixed failing tests

---
+++ frontend/src/features/dashboard/routes/__tests__/Dashboard.test.js
@@ -0,0 +1,9 @@
+import {render, screen} from "@testing-library/react"
+import { Dashboard } from "../Dashboard"
+
+describe("Test Dashboard Page", () =>
+    test("dummy Dashboard test", async () => {
+        // TODO: finish Dashboard page before adding tests
+        expect(true).toBeTruthy();
+    })
+)
---
+++ frontend/src/features/landing/routes/__tests__/Landing.test.js
@@ -0,0 +1,9 @@
+import {render, screen} from "@testing-library/react"
+import { Landing } from "../Landing"
+
+describe("Test Landing Page", () =>
+    test("dummy Landing test", async () => {
+        // TODO: finish Landing page before adding tests
+        expect(true).toBeTruthy();
+    })
+)
