72c901dccf16b2caeafa24ba0f1cd11ea51e773e
Simon Studen <simonstuden@gmail.com>
2023-03-01 15:54:20 -0800

    Styled Social Feed page and CreatePost component

--- frontend/src/features/social/components/CreatePost.jsx
+++ frontend/src/features/social/components/CreatePost.jsx
@@ -1,5 +1,8 @@
 import {useState} from "react";
 
+import { PermMedia, Label} from "@mui/icons-material";
+import { Avatar, Paper } from "@mui/material";
+
 
 export const CreatePost = (uid) => {
 
@@ -30,10 +33,28 @@ export const CreatePost = (uid) => {
     }
 
     return (
-        <div>
-            <input type="text" onChange={handleChange} value={postText}/>
-            <input type="submit" value="Post" onClick={handleSubmit}/>
-        </div>
+        <Paper sx={{ width: '100%', height: '170px', borderRadius: '10px' }}>
+          <div style={{ padding: "10px" }}>
+            <div style={{  display: 'flex', alignItems: 'center' }}> 
+              <Avatar sx={{ height: "50px", width:'50px', objectFit: 'cover', marginRight: '10px' }} />
+              <input style={{border: 'none', width: '80%' }} placeholder="What's on your mind?" onChange={handleChange} value={postText}/>
+              <input style={{border: 'none', padding: '7px', marginRight: '20px', borderRadius: '5px', backgroundColor: '#5bebbd', color: 'white', cursor: 'pointer' }} type="submit" value="Post" onClick={handleSubmit}/>
+            </div>
+            <hr style={{ margin: '20px' }}></hr>
+            <div style={{  display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}> 
+              <div style={{  display: 'flex', marginLeft: '20px'}} >
+                <div style={{ display: 'flex', alignItems: 'center', marginRight: '15px', cursor: 'pointer' }}>
+                  <PermMedia sx={{ fontSize: '18px', marginRight: '3px', color: '#03b1fc' }}/>
+                  <span style={{ fontSize: '15px', fontWeight: '500'}}>Photo/Video</span>
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', marginRight: '15px', cursor: 'pointer' }}>
+                  <Label sx={{ fontSize: '18px', marginRight: '3px', color: '#fc4103' }}/>
+                  <span style={{ fontSize: '15px', fontWeight: '500'}}>Tag</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Paper>
     )
 
 }
--- frontend/src/features/social/routes/Social.jsx
+++ frontend/src/features/social/routes/Social.jsx
@@ -1,12 +1,10 @@
-import { Container } from "@mui/system";
-import { TestComponent } from "../components/TestComponent";
+import { ContentLayout} from "../../../components/Layout/ContentLayout"
 import {CreatePost} from "../components/CreatePost";
 
 export const Social = () => {
   return (
-    <Container>
-      <h1>Social Page coming soon</h1>
+    <ContentLayout title={'Social Feed'}>
       <CreatePost />
-    </Container>
+    </ContentLayout>
   );
 }
