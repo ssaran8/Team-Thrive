92ba8a0fa62249394b1eb68dee7ae1278ff0d73e
Roy Liu <royliurl@gmail.com>
2023-03-03 18:31:21 -0800

    update edit form

---
+++ frontend/src/features/settings/components/EditForm.jsx
@@ -0,0 +1,126 @@
+import { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Avatar from "@mui/material/Avatar"
+import { Dialog, DialogContent, IconButton, DialogTitle, DialogActions } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { Container } from "@mui/system";
+import { getAuth, updateProfile } from "firebase/auth";
+
+
+export const EditForm = ({open, onClose}) => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [phone, setPhone] = useState();
+  const [photo, setPhoto] = useState();
+  
+  useEffect(() => {
+    if (open) {
+      const user = getAuth().currentUser;
+      setName(user.displayName);
+      setEmail(user.email);
+      setPhone(user.phoneNumber);
+      setPhoto(user.photoURL)
+    }
+  }, [open]);
+
+  const handleClose = () => {
+    onClose();
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, phone)
+    updateProfile(getAuth().currentUser, {
+      displayName: name,
+      email: email,
+      phoneNumber: phone,
+      photoURL: photo
+    }).then(() => {
+      alert("Succesfully updated!");
+      onClose();
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const onPhoneChange = (e) => {
+    setPhone(e.target.value);
+  }
+
+  return (
+    <Dialog
+    open={open} 
+    onClose={handleClose}
+    fullWidth
+  >
+    <DialogTitle sx={{textAlign: 'center'}}>Edit Information </DialogTitle>
+    <DialogContent>
+      <Container
+        sx={{
+          padding: 2,
+          '> *': {
+            width: '100%',
+            margin: 1,
+            p:0,
+          },
+          boxSizing: 'border-box'
+        }}
+      >
+    
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Name"
+            value={name}
+            onChange={onNameChange}
+          />
+        </div>
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            value={email}
+            onChange={onEmailChange}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Phone Number"
+            value={phone}
+            onChange={onPhoneChange}
+          />
+        </div>
+
+        <div>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" type="submit">Save Changes</Button>
+          </Stack>
+        </div>
+      </Box>
+      </Container>
+    </DialogContent>
+  </Dialog>
+  )
+}
--- frontend/src/features/settings/routes/Settings.jsx
+++ frontend/src/features/settings/routes/Settings.jsx
@@ -1,14 +1,26 @@
 import { Container } from "@mui/system";
-import { TestComponent } from "../components/TestComponent";
 import { ContentLayout } from '../../../components/Layout/ContentLayout';
+import { IconButton } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { EditForm } from "../components/EditForm";
+import { useState } from "react";
 
 
 export const Settings = () => {
+  const [open, setOpen] =  useState(false);
+
+  const onClickEdit = () => {
+    setOpen(true);
+  }
+
   return (
     <ContentLayout title={'Settings'}>
       <Container>
-        <h1>Profile</h1>
-        <TestComponent />
+        hello
+        <IconButton component="label" onClick={onClickEdit}>
+          <EditIcon />
+        </IconButton>
+        <EditForm open={open} onClose={() => setOpen(false)} />
       </Container>
     </ContentLayout>
   );
