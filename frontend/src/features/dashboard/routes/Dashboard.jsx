f93ffef1a1101f37452e75b0cb3ba3a1dcd7103c
Roy Liu <royliurl@gmail.com>
2023-03-05 07:37:16 -0800

    add dashboard changes

--- frontend/src/features/dashboard/routes/Dashboard.jsx
+++ frontend/src/features/dashboard/routes/Dashboard.jsx
@@ -1,76 +1,75 @@
 
-import { Box, Grid, Paper, Container, LinearProgress, Typography} from '@mui/material';
+import { Box, Grid, Paper, Container, LinearProgress, Typography, CircularProgress } from '@mui/material';
 
 import { TaskList } from '../components/TaskList';
 import { History } from '../components/History';
 import { ContentLayout } from '../../../components/Layout/ContentLayout';
-import { TasksContext } from '../../../routes/protected';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
 import axios from 'axios';
 import { styled } from '@mui/system';
 
 
+export const TasksContext = createContext([]);
 
+export const numTasksDone = (tasks) => {
+  return tasks.reduce((acc, task) => acc + task.done, 0);
+}
 
 export const Dashboard = () => {
   const [tasks, setTasks] = useState([]);
-  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:4567/tasks', { params: {
-      uid: getAuth().currentUser.uid,
-      scope: "all"  // TODO: change to "Today"
-    }}).then((res) => {
+    axios.get('http://localhost:4567/tasks', {
+      params: {
+        uid: getAuth().currentUser.uid,
+        scope: "today"
+      }
+    }).then((res) => {
       setTasks(res.data);
-      console.log(tasks);
+      setLoading(false);
     });
   }, [])
-  
-
-  // const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  //   height: 10,
-  //   borderRadius: 5,
-  // }));
-
 
   return (
-    <ContentLayout title={'Dashboard'}> 
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center', 
-          mb: 2
-        }}
-      >
-        <LinearProgress 
-          variant='determinate' 
-          value={progress} 
-          onClick={()=> {setProgress(progress === 100 ? 0 : progress + 10)}}
+    <ContentLayout title={'Dashboard'}>
+      <TasksContext.Provider value={{tasks, setTasks}}>
+        <Box
           sx={{
-            height: 25,
-            borderRadius: 5,
-            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mb: 2
           }}
-        />
-        <Typography variant='h6' align='center' sx={{position: 'absolute', right: 90}}> {`${progress}/100`} </Typography>
-      </Box>
-      <Grid 
-        container 
-        spacing={4}
-        sx={{
-          height: '100%',
-          alignSelf: 'center',
-        }}
-      >
-        <Grid item xs={8}>
-          <TaskList tasks={tasks}/>
+        >
+          <LinearProgress
+            variant={loading ? 'indeterminate' : 'determinate'}
+            value={Math.ceil(numTasksDone(tasks) / tasks.length * 100)}
+            sx={{
+              height: 25,
+              borderRadius: 5,
+              width: '100%',
+            }}
+          />
+          <Typography variant='h6' align='center' sx={{ position: 'absolute', right: 90 }}> {`${Math.ceil(numTasksDone(tasks) / tasks.length * 100) || 0}/100`} </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={4}
+          sx={{
+            height: '100%',
+            alignSelf: 'center',
+          }}
+        >
+          <Grid item xs={8}>
+            <TaskList tasks={tasks} loading={loading} />
+          </Grid>
+          <Grid item xs={4}>
+            <History loading={loading} />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <History />
-        </Grid>
-      </Grid>
-    </ContentLayout> 
+      </TasksContext.Provider>
+    </ContentLayout>
   )
 }
