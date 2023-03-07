852098099f488f16e2a0d21aa117517359dd418b
Roy Liu <royliurl@gmail.com>
2023-03-07 15:30:26 -0800

    add reroute for non-auth

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
 
 import { Landing } from '../features/landing';
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,7 +8,10 @@ import { protectedRoutes } from './protected';
 
 export const AppRoutes = () => {
   const [user, loading, error] = useAuthState(auth);
-  
-  const routes = user ? protectedRoutes : [{ path: '/', element: <Landing /> }];
+
+  const routes = user ? protectedRoutes : [
+    { path: '/', element: <Landing /> },
+    { path: '*', element: <Navigate to='/' /> }
+  ];
   return <>{useRoutes(routes)}</>;
 };
